96d2240956866b9c1b75d8fb056331e6df279506
ashu4587 <57616043+ashu4587@users.noreply.github.com>
2020-10-26 11:36:46 +0530

    login

---
+++ login/connection/connection.php
@@ -0,0 +1,15 @@
+<?php
+  define('HOST', 'localhost');
+  define('USER', 'root');
+  define('PASS', '');
+  define('DATA', 'data');
+  define('TABLE', 'data');
+
+  $mysql= mysqli_connect(HOST, USER, PASS, DATA);
+  if (mysqli_connect_error()){
+    echo "failed to connect to database".mysqli_connect_error();
+  }else{
+    echo "you are sucessfully connected to database";
+  }
+
+?>
---
+++ login/form-login.php
@@ -0,0 +1,24 @@
+<html>
+  <head> <title> login </title>
+  </head>
+  <body>
+    <link rel="stylesheet" href="./css/style.css">
+    <div style="width:800px; margin:0 auto;" class="col-sm-5" style="align:center;">
+    <form action="" method="post">
+      <div class="imgcontainer">
+        <img src="./img/img_avatar2.img" alt="Avatar" class="avatar">
+      </div>
+
+      <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="username" >
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" >
+
+        <button type="submit">Login</button>
+        
+    </form>
+    </div>
+</body>
+</html>
